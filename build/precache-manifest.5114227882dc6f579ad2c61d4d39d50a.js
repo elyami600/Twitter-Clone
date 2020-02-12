d8a96c5811e56bfae098dedf600f65a295867e8b
Yamil Perez <42903867+elyami600@users.noreply.github.com>
2020-02-12 14:54:40 -0500

    new file added

---
+++ build/precache-manifest.5114227882dc6f579ad2c61d4d39d50a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f2359d4426aea56941ae25be66313d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "199c340538f1bbdc4702",
+    "url": "/static/css/main.460b0ac9.chunk.css"
+  },
+  {
+    "revision": "7ad1fb9596be3830ef0c",
+    "url": "/static/js/2.92858f14.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.92858f14.chunk.js.LICENSE"
+  },
+  {
+    "revision": "199c340538f1bbdc4702",
+    "url": "/static/js/main.c5883a80.chunk.js"
+  },
+  {
+    "revision": "7f3e46e2bd0271af4afe",
+    "url": "/static/js/runtime-main.ed39efeb.js"
+  }
+]);
